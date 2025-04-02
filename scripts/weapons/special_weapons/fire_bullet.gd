142d3d7dc7493fbe99b8e410e745a40dadd89aaa
Dennisbobi <dennis.danskgamer@gmail.com>
2025-04-02 09:13:25 +0200

    små ændringer til ice cannon

--- scripts/weapons/special_weapons/fire_bullet.gd
+++ scripts/weapons/special_weapons/fire_bullet.gd
@@ -23,6 +23,7 @@ func _physics_process(delta):
 	scale += Vector2(0.01, 0.01)
 	if BulletSpeed > 125:
 		BulletSpeed -= 1
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemy"):
