83e96363ad34bfbe4237a64c3c3f2f39282d86ff
Kryzi <magnus.krydsfeldt@hotmail.com>
2025-04-08 11:54:04 +0200

    UI fix i main menu

--- scripts/main/main_menu/settings.gd
+++ scripts/main/main_menu/settings.gd
@@ -1,5 +1,29 @@
 extends Node
 
+@onready var Logo = $Logo
+@onready var Bg = $Background
+var logo_up = true #bevæger loget sig op?
+var i = 0
+var logo_movement = 100
+
+#func _ready():
+	#var win = get_viewport().size
+	#Bg.size = win
+	#Logo.size = win
+
+func _process(_delta):
+	if logo_up == false:
+		Logo.position -= Vector2(0, 0.1)
+		i += 1
+		if i > logo_movement:
+			i = 0
+			logo_up = true
+	else:
+		Logo.position += Vector2(0, 0.1)
+		i += 1
+		if i > logo_movement:
+			i = 0
+			logo_up = false
 
 func _on_admin_pressed() -> void:
 	pass # Replace with function body.
