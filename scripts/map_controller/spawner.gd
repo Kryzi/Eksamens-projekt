a81a1cbec9135d485edcd35851245c493fdbfbb5
RazPalima <amilap.oettam@gmail.com>
2025-04-08 09:50:06 +0200

    obstacle generation

--- scripts/map_controller/spawner.gd
+++ scripts/map_controller/spawner.gd
@@ -8,7 +8,11 @@ extends Node2D
 @export var stone_obstacle: PackedScene
 @export var wood_obstacle: PackedScene
 
-@onready var map_controller = get_node("/root/Main/MapController")
+@onready var obstacle_scenes: Array[PackedScene] = [stone_obstacle, wood_obstacle]
+
+@onready var main = get_node("/root/Main/")
+@onready var map_controller = main.get_node("MapController")
+@onready var navigation_region: NavigationRegion2D = main.get_node("NavigationRegion2D")
 @onready var background = map_controller.get_node("Background")
 
 @onready var layout1 = background.get_node("Layout1")
@@ -21,12 +25,16 @@ extends Node2D
 @onready var layout1_bridge_shop = background.get_node("Layout1BridgeShop")
 @onready var layoutBoss = background.get_node("LayoutBoss")
 
-@onready var boundary = layout1.get_node("EnemyArea1/SpawnPolygon1")
-@onready var obstacle_collision_shape = layout1.get_node("ObstacleArea/CollisionShape2D")
+@onready var boundary: CollisionPolygon2D = layout1.get_node("EnemyArea1/SpawnPolygon1")
+@onready var obstacle_boundary: CollisionPolygon2D = layout1.get_node("ObstacleArea1/CollisionPolygon2D")
 
-@onready var obstacle_scene_array: Array[PackedScene] = [stone_obstacle, wood_obstacle]
-var spawnArea: Rect2  # Define a Rect2 for spawn area
+var min_obstacle_spacing: int = 300
+var generated_obstacle_positions: Array[Vector2]
+var max_single_obstacle_generation_attempts: int = 5
 var obstacleArea: Rect2
+var obstacleAreaPolygon: PackedVector2Array
+
+var spawnArea: Rect2  # Define a Rect2 for spawn area
 var rewardValue: int = 0
 #var mapValue: String = ""
 
@@ -37,52 +45,66 @@ var variationID = 0
 
 func _ready() -> void:
 	create_spawn_area(boundary)
-	'print(layout1_bridge_shop.visible )
-	if (layoutBoss.visible == false and layout1_bridge_shop.visible == false):
-		create_obstacle_area(obstacle_collision_shape)'
+	generate_obstacles(obstacle_boundary)
+	#print(layout1_bridge_shop.visible )
+	#if (layoutBoss.visible == false and layout1_bridge_shop.visible == false):
+		#create_obstacle_area(obstacle_collision_shape)
 
-#func update_navigation_obstacles():
-	#var navigation_region = $NavigationRegion2D
-	#if navigation_region is NavigationRegion2D:
-		#var nav_polygon = navigation_region.navigation_polygon
-		#for static_body in get_tree().get_nodes_in_group("obstacle"):
-			#if static_body is StaticBody2D:
-				#var collision_shape := static_body.get_child("CollisionShape2D") as CollisionShape2D
-				#if collision_shape.shape is RectangleShape2D:
-					#var rect = collision_shape.size  # Assuming RectangleShape2D
-					#var global_position = static_body.global_position
-					#nav_polygon.add_collision_polygon(Rect2(global_position - rect / 2, rect))
-
-
-func create_obstacle_area(new_obstacle_collision_shape: CollisionShape2D) -> void:
-	obstacleArea = get_obstacle_area(new_obstacle_collision_shape)
+#region Obstacle generation
+func generate_obstacles(new_obstacle_boundary: CollisionPolygon2D) -> void:
+	obstacleAreaPolygon = get_obstacle_area_polygon(new_obstacle_boundary)
+	obstacleArea = get_area_from_boundary(new_obstacle_boundary)
 	
-	var amount = randi_range(1, 10)
+	var amount = randi_range(10, 50)
+	# Reset previously generated obstacle positions
+	generated_obstacle_positions = []
 	for i in amount:
-		#print(i)
 		random_obstacle_generation()
 		
+	call_deferred("awaited_navigation_region_baking")
+
+func awaited_navigation_region_baking() -> void:
+	await get_tree().physics_frame
+	navigation_region.bake_navigation_polygon()
+
+func get_obstacle_area_polygon(collision_polygon: CollisionPolygon2D) -> PackedVector2Array:
+	# Converts the local polygon points to global positions and stores them in a PackedVector2Array
+	var global_polygon_points = PackedVector2Array()
+	for local_point in collision_polygon.polygon:
+		global_polygon_points.append(collision_polygon.to_global(local_point))
+	return global_polygon_points
+
+func random_obstacle_generation() -> void:
+	for attempt in range(max_single_obstacle_generation_attempts):
+		var rand_x = randf_range(obstacleArea.position.x, obstacleArea.end.x)
+		var rand_y = randf_range(obstacleArea.position.y, obstacleArea.end.y)
+		var pos = Vector2(rand_x, rand_y)
+		if Geometry2D.is_point_in_polygon(pos, obstacleAreaPolygon) and is_obstacle_position_valid(pos):
+			var obstacle_scene = obstacle_scenes.pick_random()
+			var obstacle_instance = obstacle_scene.instantiate()
+			obstacle_instance.position = pos
+			generated_obstacle_positions.append(pos)
+			call_deferred("add_child", obstacle_instance)
+			break
+
+func is_obstacle_position_valid(new_pos: Vector2) -> bool:
+	for obstacle_position in generated_obstacle_positions:
+		if obstacle_position.distance_to(new_pos) < min_obstacle_spacing:
+			return false
+	return true
+#endregion Obstacle generation
+
+#region Enemy spawning
 func create_spawn_area(new_boundary):
-	spawnArea = get_spawn_area(new_boundary)  # Calculate the spawn area bounding box
+	spawnArea = get_area_from_boundary(new_boundary)  # Calculate the spawn area bounding box
 	
 	#print("Spawner ready:", self.name, "from", get_parent())
 	var amount = randi_range(1, 4)
 	for i in amount:
 		#print(i)
 		random_spawn()
-
-func get_obstacle_area(collision_shape_for_obstacle_area: CollisionShape2D) -> Rect2:
-	var shape = collision_shape_for_obstacle_area.shape
-	if shape is RectangleShape2D:
-		var size = (shape as RectangleShape2D).size  # Width and height of the rectangle
-		var top_left = collision_shape_for_obstacle_area.to_global(-size / 2)
-		var bottom_right = collision_shape_for_obstacle_area.to_global(size / 2)
 		
-		return Rect2(top_left, bottom_right - top_left)  # Create Rect2 from top-left and size
-	
-	return Rect2()  # Return an empty Rect2 if shape type is unsupported
-		
-func get_spawn_area(boundary_for_spawn_area: CollisionPolygon2D) -> Rect2:
+func get_area_from_boundary(boundary_for_spawn_area: CollisionPolygon2D) -> Rect2:
 	var min_x = INF
 	var min_y = INF
 	var max_x = -INF
@@ -97,16 +119,6 @@ func get_spawn_area(boundary_for_spawn_area: CollisionPolygon2D) -> Rect2:
 
 	return Rect2(Vector2(min_x, min_y), Vector2(max_x - min_x, max_y - min_y))
 
-func random_obstacle_generation() -> void:
-	var rand_x = randf_range(obstacleArea.position.x, obstacleArea.end.x)
-	var rand_y = randf_range(obstacleArea.position.y, obstacleArea.end.y)
-	var pos = Vector2(rand_x, rand_y)
-	var obstacle_scene = obstacle_scene_array.pick_random()
-	var obstacle_instance = obstacle_scene.instantiate()
-	
-	obstacle_instance.position = pos
-	call_deferred("add_child", obstacle_instance)
-
 func random_spawn():
 	var x = randf_range(spawnArea.position.x, spawnArea.end.x)
 	var y = randf_range(spawnArea.position.y, spawnArea.end.y)
@@ -119,6 +131,7 @@ func random_spawn():
 	var enemy_instance = selected_enemy.instantiate()
 	enemy_instance.position = pos
 	call_deferred("add_child", enemy_instance)
+#endregion Enemy Spawning
 
 func rewardSet(value):
 	#rewardValue = randi_range(1, 3)
@@ -168,7 +181,7 @@ func getItem():
 	itemInstance.itemGenerator()  # Now you can call itemGenerator() on the instance
 	# teleportere tilfældigt item til midten af boundry
 	#var polygon = boundary.polygon 
-	spawnArea = get_spawn_area(boundary)
+	spawnArea = get_area_from_boundary(boundary)
 	var x = (spawnArea.position.x + spawnArea.end.x)/randf_range(1.25,2.75)
 	#var y = (spawnArea.position.y + spawnArea.end.y)/2
 	var y = (spawnArea.position.y + spawnArea.end.y)/randf_range(1.25,2.75)
@@ -270,10 +283,10 @@ func checkDeath():
 			
 		if layout3.visible == true:
 			layout3.visible = false
-			layout3.get_node("Boundary3_1/CollisionPolygon2D").disabled = true
+			layout3.get_node("Boundary3/CollisionPolygon2D").disabled = true
 			layout3_bridge.visible = true
 			
-			layout3_bridge.get_node("Variation3_1/TeleporterArea1/Teleporter1").disabled = false
+			layout3_bridge.get_node("Variation3_1/TeleporterArea3_1/Teleporter3_1").disabled = false
 			layout3_bridge.get_node("Variation3_2").visible = false
 			layout3_bridge.get_node("Variation3_3").visible = false
 			
