3529f5e10a29d8a9135bf6d81b7558e2046ce84d
Dennisbobi <dennis.danskgamer@gmail.com>
2025-04-09 11:08:38 +0200

    godot enemy 1.1

---
+++ scripts/enemies/enemy_1_1.gd
@@ -0,0 +1,111 @@
+extends CharacterBody2D
+
+var speed = 8000
+var health = 23
+var Enemy_state: String = "Walking"
+var last_direction: Vector2 = Vector2.DOWN
+var player_in_attack_range = false  # Holder styr på, om spilleren er tæt nok på til angreb
+
+@onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var player = get_node("/root/Main/Player")
+@onready var controller = get_node("/root/Main/MapController/Spawner")
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var attack_area = $View  # Area2D der registrerer, om spilleren er i skud-rækkevidde
+@onready var ShootSound = $Enemy1Sound
+
+@export var bullet_scene: PackedScene
+@export var other_bullet_scene: PackedScene
+@export var coin_scene: PackedScene
+
+func _physics_process(delta) -> void:
+	# Fjenden bevæger sig kun, hvis spilleren ikke er i angrebsradius
+	if not player_in_attack_range:
+		var dir = to_local(nav_agent.get_next_path_position()).normalized()
+
+		# Opdater Enemy_state baseret på bevægelse
+		if dir.length() > 0:
+			Enemy_state = "Walking"
+			last_direction = dir
+		else:
+			Enemy_state = "Idle"
+
+		velocity = dir * speed * delta
+		play_anim(dir)  # Opdater animation baseret på retning
+		
+		move_and_slide()
+	else:
+		Enemy_state = "Idle"  # Stå stille mens den skyder
+		velocity = Vector2.ZERO
+
+func play_anim(dir):
+	if Enemy_state == "Idle":
+		animated_sprite.play("Idle " + Aimdirection(last_direction))
+	elif Enemy_state == "Walking":
+		animated_sprite.play("Walk " + Aimdirection(dir))
+
+func Aimdirection(dir: Vector2) -> String:
+	if abs(dir.x) > abs(dir.y):
+		return "højre" if dir.x > 0 else "venstre"
+	else:
+		return "ned" if dir.y > 0 else "op"
+
+func make_path() -> void:
+	nav_agent.target_position = player.global_position  # Altid gå mod spilleren
+
+func _on_move_timer_timeout():
+	make_path()
+
+func die():
+	dropCoin()
+	queue_free()
+	controller.checkDeath()
+
+func dropCoin():
+	var coin_instance = coin_scene.instantiate()
+	coin_instance.global_position = global_position
+	get_tree().get_root().call_deferred("add_child", coin_instance)
+
+func hit_damage(damage):
+	health -= damage
+	if health <= 0:
+		die()
+
+func shoot():
+	var direction_to_player = (player.global_position - global_position).normalized()
+	
+	var left_offset = direction_to_player.rotated(deg_to_rad(20))
+	var right_offset = direction_to_player.rotated(deg_to_rad(-20))
+	
+	
+	if player_in_attack_range:
+		ShootSound.play()
+		animated_sprite.play("Angreb " + Aimdirection(last_direction))
+		
+		var bullet_instance = bullet_scene.instantiate()
+		bullet_instance.global_position = $ShootingPoint.global_position
+		bullet_instance.rotation = rotation
+		bullet_instance.targetPos = player.global_position  
+		get_tree().get_root().call_deferred("add_child", bullet_instance)  
+		
+		var bullet_instance2 = other_bullet_scene.instantiate()
+		bullet_instance2.global_position = $ShootingPoint.global_position
+		bullet_instance2.direction = left_offset
+		get_tree().get_root().call_deferred("add_child", bullet_instance2) 
+		
+		var bullet_instance3 = other_bullet_scene.instantiate()
+		bullet_instance3.global_position = $ShootingPoint.global_position
+		bullet_instance3.direction = right_offset
+		get_tree().get_root().call_deferred("add_child", bullet_instance3) 
+
+func _on_attack_timer_timeout() -> void:
+	if player_in_attack_range:  # Kun skyd, hvis spilleren er inden for rækkevidde
+		shoot()
+		await animated_sprite.animation_finished
+
+func _on_view_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):  # Sørg for at spilleren er i en gruppe "Player"
+		player_in_attack_range = true
+
+func _on_view_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_attack_range = false
