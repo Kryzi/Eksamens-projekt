8c228d1db49673b317f43905655bb0cebc62a0cf
StorHage <davidfredsted@gmail.com>
2025-04-09 15:07:18 +0200

    fic

--- globals/player_info.gd
+++ globals/player_info.gd
@@ -5,7 +5,7 @@ signal ammo_changed(new_ammo_data: Dictionary)
 signal coin_count_changed(new_coins: int)
 signal win_screen_reached(is_player_victorious: bool,coins_high_score: int)
 
-'var timer: int = 0'
+# Bruges til at bestemme hvem har klaret spillet hurtigst
 var timer = 0
 var current_coins: int = 0:
 	set(value):
