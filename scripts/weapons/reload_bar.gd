38d7c980ec8425645e8c099e21687afbf7f82580
Dennisbobi <dennis.danskgamer@gmail.com>
2025-04-11 08:54:12 +0200

    auto bue

--- scripts/weapons/reload_bar.gd
+++ scripts/weapons/reload_bar.gd
@@ -5,6 +5,7 @@ var reload_timer: float = 0.0
 
 func _ready() -> void:
 	visible = false
+	max_value = 0
 
 func weaponChanged():
 	if weapon.weapons[weapon.currentWeapon].name == "Boomerang (weapon)": # band aid solution
@@ -14,6 +15,7 @@ func weaponChanged():
 		max_value = weapon.weapons[weapon.currentWeapon].reloadTime
 		value = 0
 		reload_timer = 0
+		
 		print("Reload time set to: ", max_value)
 		set_process(true)
 	else:
@@ -23,8 +25,10 @@ func _process(delta: float) -> void:
 	var current_weapon = weapon.weapons[weapon.currentWeapon]
 	if current_weapon.ranged == true:
 		if current_weapon.reloading == true and not current_weapon.is_in_group("Boomerang"):
-			
+			print(max_value)
 			if current_weapon.reserveAmmo == 0 and current_weapon.currentAmmo == 0:
+				return
+			if max_value <= 0:
 				return
 			
 			visible = true
