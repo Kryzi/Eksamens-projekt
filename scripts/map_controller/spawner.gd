22b78a271ed638602995b62fdc49551e926f6ef7
StorHage <davidfredsted@gmail.com>
2025-04-11 14:21:46 +0200

    shop altid før boss

--- scripts/map_controller/spawner.gd
+++ scripts/map_controller/spawner.gd
@@ -317,6 +317,7 @@ func checkDeath():
 		if (PlayerInfo.bossTimer > 12):
 			rewardSet(3)
 			variationID = 1
+			print("boss shop used")
 		#Sikre at man alitd for coins i starten
 		if (PlayerInfo.bossTimer < 3):
 			rewardSet(1)
@@ -344,7 +345,7 @@ func checkDeath():
 				layout1_bridge.get_node("Variation1_2/TeleporterArea1_2/Teleporter1_2").disabled = false
 				
 				rewardSet(randi_range(1, 3))
-				if (PlayerInfo.bossTimer == 11):
+				if (PlayerInfo.bossTimer == 12):
 					rewardSet(randi_range(1, 2))
 				layout1_bridge.get_node("Variation1_2/rewardLabel1_2").text = PlayerInfo.mapValue
 				layout1_bridge.get_node("Variation1_2/rewardIcon").play(PlayerInfo.mapValue)
@@ -360,7 +361,7 @@ func checkDeath():
 					layout1_bridge.get_node("Variation1_3/TeleporterArea1_3/Teleporter1_3").disabled = false
 					
 					rewardSet(randi_range(1, 3))
-					if (PlayerInfo.bossTimer == 11):
+					if (PlayerInfo.bossTimer == 12):
 						rewardSet(randi_range(1, 2))
 					layout1_bridge.get_node("Variation1_3/rewardLabel1_3").text = PlayerInfo.mapValue
 					layout1_bridge.get_node("Variation1_3/rewardIcon").play(PlayerInfo.mapValue)
@@ -391,7 +392,7 @@ func checkDeath():
 				layout2_bridge.get_node("Variation2_2/BoundaryBridge2_2/CollisionPolygon2D").disabled = false
 				layout2_bridge.get_node("Variation2_2/TeleporterArea2_2/Teleporter2_2").disabled = false
 				rewardSet(randi_range(1, 3))
-				if (PlayerInfo.bossTimer == 11):
+				if (PlayerInfo.bossTimer == 12):
 					rewardSet(randi_range(1, 2))
 				layout2_bridge.get_node("Variation2_2/rewardLabel2_2").text = PlayerInfo.mapValue
 				layout2_bridge.get_node("Variation2_2/rewardIcon").play(PlayerInfo.mapValue)
@@ -406,7 +407,7 @@ func checkDeath():
 					layout2_bridge.get_node("Variation2_3/TeleporterArea2_3/Teleporter2_3").disabled = false
 					
 					rewardSet(randi_range(1, 3))
-					if (PlayerInfo.bossTimer == 11):
+					if (PlayerInfo.bossTimer == 12):
 						rewardSet(randi_range(1, 2))
 					layout2_bridge.get_node("Variation2_3/rewardLabel2_3").text = PlayerInfo.mapValue
 					layout2_bridge.get_node("Variation2_3/rewardIcon").play(PlayerInfo.mapValue)
@@ -435,7 +436,7 @@ func checkDeath():
 				layout3_bridge.get_node("Variation3_2/TeleporterArea3_2/Teleporter3_2").disabled = false
 				
 				rewardSet(randi_range(1, 3))
-				if (PlayerInfo.bossTimer == 11):
+				if (PlayerInfo.bossTimer == 12):
 					rewardSet(randi_range(1, 2))
 				layout3_bridge.get_node("Variation3_2/rewardLabel3_2").text = PlayerInfo.mapValue
 				layout3_bridge.get_node("Variation3_2/rewardIcon").play(PlayerInfo.mapValue)
@@ -450,7 +451,7 @@ func checkDeath():
 					layout3_bridge.get_node("Variation3_3/TeleporterArea3_3/Teleporter3_3").disabled = false
 					
 					rewardSet(randi_range(1, 3))
-					if (PlayerInfo.bossTimer == 11):
+					if (PlayerInfo.bossTimer == 12):
 						rewardSet(randi_range(1, 2))
 					layout3_bridge.get_node("Variation3_3/rewardLabel3_3").text = PlayerInfo.mapValue
 					layout3_bridge.get_node("Variation3_3/rewardIcon").play(PlayerInfo.mapValue)
